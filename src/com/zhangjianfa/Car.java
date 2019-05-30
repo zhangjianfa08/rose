c649c56ea22cf914217a6857347f6e60c560827c
zhangjianfa <zjfaizyy@163.com>
2019-05-30 15:27:15 +0800

    First commit all.

---
+++ src/com/zhangjianfa/Car.java
@@ -0,0 +1,11 @@
+package com.zhangjianfa;
+
+public class Car {
+    int color;
+    int speed;
+    String title;
+    String content;
+    void startup(){
+        System.out.println("啟動！");
+    }
+}
